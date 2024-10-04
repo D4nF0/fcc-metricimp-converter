14e12fbb4d6fbe2e4350d73f155a3f677001d6c0
Daniel Fořt <danielfort2005@gmail.com>
2024-10-04 19:35:15 +0000

    feat: add api file functionality

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,31 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.get('/api/convert', (req, res) => {
+    let input = req.query.input;
+    let initUnit = convertHandler.getUnit(input);
+    let initNum = convertHandler.getNum(input);
+
+    if( !initNum && !initUnit ){
+      res.send('invalid number and unit');
+    } else if( !initNum ){
+      res.send('invalid number');
+    } else if( !initUnit ){
+      res.send('invalid unit');
+    }
+
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string,
+    })
+  });
 };
